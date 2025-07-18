27e706121183e9a3250114e0e5e0a47dd89fd6ef
Luca5Eckert <ecklukatv@gmail.com>
2025-07-18 20:19:59 -0300

    feat(lengthOfLongestSubstring): solucao do problema leet code 03
    Signed-off-by: Luca5Eckert <ecklukatv@gmail.com>

---
+++ src/lengthOfLongestSubstring/Solution.java
@@ -0,0 +1,37 @@
+package lengthOfLongestSubstring;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> setTable = new HashSet<>();
+        int longestSequence = 0;
+        int left = 0;
+
+
+        for(int right = 0; right < s.length(); right++){
+            char character = s.charAt(right);
+
+
+            while(setTable.contains(character)){
+                setTable.remove(s.charAt(left));
+                left++;
+            }
+
+
+            setTable.add(character);
+
+
+            int tempSequence = right - left + 1;
+
+
+            if(tempSequence > longestSequence){
+                longestSequence = tempSequence;
+            }
+        }
+        return longestSequence;
+    }
+}
+
