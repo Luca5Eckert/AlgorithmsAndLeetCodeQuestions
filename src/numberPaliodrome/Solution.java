dadbdf77ef863ab712e84416c8f667040dd45a39
Luca5Eckert <ecklukatv@gmail.com>
2025-07-18 21:52:50 -0300

    feat(paliodrome): solucao do problema leet code 09
    Signed-off-by: Luca5Eckert <ecklukatv@gmail.com>

---
+++ src/numberPaliodrome/Solution.java
@@ -0,0 +1,26 @@
+package numberPaliodrome;
+
+class Solution {
+    public static boolean isPalindrome(int x) {
+
+        if(x < 0){
+            return false;
+        }
+
+        int copia = x;
+        int reverso = 0;
+
+        while (copia > 0){
+            reverso = (reverso * 10) + (copia%10);
+            copia = copia / 10;
+        }
+
+        return reverso == copia;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(1234567899));
+    }
+
+
+}
