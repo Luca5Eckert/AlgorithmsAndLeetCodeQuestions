172da689ce4a16122623342345921b2121f3ee3a
Luca5Eckert <ecklukatv@gmail.com>
2025-07-24 19:41:04 -0300

    feat: resolvido a questão 1717 : Move Zeroes
    QUESTAO 283 - Move Zeroes
    Dificuldade: Facil
    Objetivo: Pegar um array de inteiros e transferir todos os números 0 para o final
    
    Signed-off-by: Luca5Eckert <ecklukatv@gmail.com>

---
+++ src/moveZeroes/Solution.java
@@ -0,0 +1,43 @@
+package moveZeroes;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public void moveZeroes(int[] nums) {
+        int contadorNumeros = 0;
+        int n = nums.length;
+
+        for(int i = 0; i < n; i++){
+            int number = nums[i ];
+
+            if(number != 0){
+                nums[contadorNumeros] = number;
+                contadorNumeros++;
+            }
+        }
+
+        for(int i = contadorNumeros; i < nums.length; i++){
+            nums[i] = 0;
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] nums1 = {0,1,0,3,12};
+
+        int[] resultExpected = {1,3,12,0,0};
+
+        s.moveZeroes(nums1);
+
+        if(Arrays.equals(nums1, resultExpected)){
+            System.out.println("Correct");
+        } else {
+            System.out.println("Incorrect");
+        }
+
+    }
+}
