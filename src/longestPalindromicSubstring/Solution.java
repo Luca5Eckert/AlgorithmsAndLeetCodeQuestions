9773425c4339506a4d99ee9a2b8e68d5bf235faf
Luca5Eckert <ecklukatv@gmail.com>
2025-07-28 15:32:29 -0300

    feat: resolvido a questão 5 : Longest Palindromic Substring
    QUESTAO 5 - Longest Palindromic Substring
    Dificuldade: Médio
    Objetivo: Encontrar a maior sub string que é um palíndromo dentro da String
    
    Signed-off-by: Luca5Eckert <ecklukatv@gmail.com>

---
+++ src/longestPalindromicSubstring/Solution.java
@@ -0,0 +1,48 @@
+package longestPalindromicSubstring;
+
+public class Solution {
+
+    public String longestPalindrome(String s) {
+        int n = s.length()-1;
+        String longestString = s.substring(0,1);
+
+        for(int i = 0; i < n; i++){
+            String odd = expenseString(s,i,i);
+            String even = expenseString(s,i,i+1);
+
+            if(odd.length() > longestString.length()){
+                longestString = odd;
+            }
+            if(even.length() > longestString.length()){
+                longestString = even;
+            }
+        }
+        return longestString;
+    }
+
+    public String expenseString(String s, int left, int right){
+
+        while((left >= 0 && right < s.length()) && (s.charAt(left) == s.charAt(right))){
+            left--;
+            right++;
+        }
+
+        return s.substring(left+1,right);
+
+    }
+
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        String test = "bb";
+
+        String resultExpected = "bb";
+
+        String result = s.longestPalindrome(test);
+
+        System.out.println("RESULT EXPECTED: " + resultExpected);
+        System.out.println("RESULT: " + result );
+
+    }
+}
