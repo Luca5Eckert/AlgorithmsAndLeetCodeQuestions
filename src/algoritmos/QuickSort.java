8b899680c5af0d0ca62bf829ac2f3ed39ac32cd8
Luca5Eckert <ecklukatv@gmail.com>
2025-11-11 16:11:59 -0300

    refactor: retira >= para otimização

--- src/algoritmos/QuickSort.java
+++ src/algoritmos/QuickSort.java
@@ -16,37 +16,33 @@ public class QuickSort {
 
     private int partition(int[] array, int low, int high) {
         int pivot = array[high];
-        int i = low - 1; // Index of smaller element
+        int i = low - 1;
 
-        // Iterate j from 'low' up to (but not including) 'high' (the pivot)
         for(int j = low; j < high; j++){
-            if(array[j] <= pivot){
-                i++; // Increment index of smaller element
+            if(array[j] < pivot){
+                i++;
 
-                // Swap array[i] and array[j]
                 int temp = array[i];
                 array[i] = array[j];
                 array[j] = temp;
             }
         }
 
-        // Swap the pivot element (array[high]) with the element at array[i+1]
         int temp = array[i+1];
         array[i+1] = array[high];
         array[high] = temp;
 
-        return i+1; // Return the final partition index
+        return i+1;
     }
 
 
     public static void main(String[] args) {
         QuickSort sort = new QuickSort();
 
-        int[] array = new int[]{1,3,4,5,2,1};
+        int[] array = new int[]{1,3,4,5,2,4,4,32,32,3,2,32,42,43,43,2,4,23,2,43,243,42,1};
 
-        sort.sort(array, 0, 5);
+        sort.sort(array, 0, array.length - 1);
 
-        // Correct way to print the array contents
         System.out.println(Arrays.toString(array));
     }
 }
