0fd067bb23cab3cd326b81c1ba34896dca757d80
Luca5Eckert <ecklukatv@gmail.com>
2025-12-09 15:34:21 -0300

    feat: adiciona solução da questão 228. Summary Ranges

---
+++ src/leetcode/summaryRanges/Solution.java
@@ -0,0 +1,33 @@
+package leetcode.summaryRanges;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> ranges = new ArrayList<>();
+
+        if(nums == null || nums.length == 0){
+            return ranges;
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            int start = nums[i];
+
+            while(i + 1 != nums.length && nums[i] + 1 == nums[i+1]){
+                i++;
+            }
+
+            if(start != nums[i]){
+                ranges.add(start + "->" + nums[i]);
+            } else {
+                ranges.add(String.valueOf(start));
+            }
+
+        }
+
+        return ranges;
+
+    }
+
+}
